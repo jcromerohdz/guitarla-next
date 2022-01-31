9b28e8e261d5d039be14b683b00a886481d9c4ea
jcromerohdz <jcromerohdz@gmail.com>
2022-01-31 12:12:39 -0800

    Adding styles to Header component via css styles modules

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,24 @@
-import Link from "next/link"; 
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Header.module.css"
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <div className='contenedor'>
-        <div>
+        <div className={styles.bar}>
+          <Link href="/">
+            <Image width={400} height={100} src="/img/logo.svg" alt="logo" />
+          </Link>
 
+          <nav className={styles.navigation}>
+            <Link href="/">Home</Link>
+            <Link href="/aboutus">About Us</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/store">Store</Link>
+
+          </nav>
         </div>
-
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/aboutus">About Us</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/store">Store</Link>
-
-        </nav>
 
       </div>
 
