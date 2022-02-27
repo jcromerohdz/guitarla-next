4507a48b72d8202a14a58aa97063decd2ba6703c
jcromerohdz <jcromerohdz@gmail.com>
2022-02-27 10:33:21 -0800

    Consuming API via Next build in getServerSideProps()

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({feeds}) => {
+
+  // useEffect(() => {
+  //   const queryBlogs = async () => {
+  //     const url = "http://localhost:1337/api/blogs?populate=*"
+  //     const response = await fetch(url)
+  //     const result = await response.json()
+
+  //     console.log(result)
+  //   }
+
+  //   queryBlogs()
+
+  // }, [])
+  console.log(feeds)
+
   return (
     <Layout
       page={'Blog'}
@@ -9,5 +25,19 @@ const Blog = () => {
     </Layout>
   );
 };
+
+export async function getServerSideProps(){
+
+  const url = "http://localhost:1337/api/blogs?populate=*"
+  const response = await fetch(url)
+  const feeds = await response.json()
+
+  console.log(feeds)
+  return {
+    props: {
+      feeds: feeds
+    }
+  }
+}
 
 export default Blog;
